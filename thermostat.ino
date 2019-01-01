5b138b83b24c6b73b8cbc59264a110522990c681
Michal Artazov <michal@artazov.cz>
2019-01-01 14:03:28 +0100

    use buttons to set temperature and display it on LCD

--- thermostat.ino
+++ thermostat.ino
@@ -1,17 +1,63 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
+#define DEFAULT_DEGREES 20
+
+#define BUTTON_DECREMENT_PIN 12
+#define BUTTON_INCREMENT_PIN 11
+
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int degreesCelsius = DEFAULT_DEGREES;
+bool buttonDecrementDown = false;
+bool buttonIncrementDown = false;
+
+bool isButtonPushed(int pin) {
+  int buttonRead = digitalRead(pin);
+  return buttonRead == LOW;
+}
+
+void refreshDisplay() {
+  lcd.clear();
+  lcd.print(degreesCelsius);
+  lcd.print(" C");
+}
 
 void setup()
 {
 	// initialize the LCD
 	lcd.begin();
   lcd.backlight();
+
+  // Define pin as input and activate the internal pull-up resistor
+  pinMode(BUTTON_DECREMENT_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_INCREMENT_PIN, INPUT_PULLUP);
 }
 
 void loop()
 {
-	// TODO
+	bool isDecrementButtonPushed = isButtonPushed(BUTTON_DECREMENT_PIN);
+  bool isIncrementButtonPushed = isButtonPushed(BUTTON_INCREMENT_PIN);
+  
+  if (isDecrementButtonPushed) {
+    if (!buttonDecrementDown) {
+      degreesCelsius--;
+      buttonDecrementDown = true;
+    }
+  } else {
+    buttonDecrementDown = false;
+  }
+
+  if (isIncrementButtonPushed) {
+    if (!buttonIncrementDown) {
+      degreesCelsius++;
+      buttonIncrementDown = true;
+    }
+  } else {
+    buttonIncrementDown = false;
+  }
+
+  refreshDisplay();
+  delay(100);
 }
